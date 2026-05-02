0f907466daebc73e96c1f86a8764fa766a57d8b6
Alen Karnil <alankarnil@gmail.com>
2026-05-02 11:14:28 +0100

    cleanup

--- ESP32BluetoothAudioControls.ino
+++ ESP32BluetoothAudioControls.ino
@@ -1,16 +1,20 @@
 #include <BleKeyboard.h>
 
+// Requires https://github.com/T-vK/ESP32-BLE-Keyboard library 0.3.2
+// Requires esp32 arduino bsp 3.0.2
+
+
 // For Nologo ESP32C3 Super Mini
 
 typedef struct debounce
 {
-  int buttonState;
-  int lastButtonState;
-  unsigned long lastDebounceTime;  
+  int button_state;
+  int last_button_state;
+  unsigned long last_debounce_time;  
 } debounce_t;
 
-debounce_t default_debounce = { LOW, LOW, 0};
-unsigned long debounceDelay = 50;
+debounce_t dd = { LOW, LOW, 0};
+unsigned long debounce_delay = 50;
 
 typedef struct pin_to_key
 {
@@ -21,18 +25,19 @@ typedef struct pin_to_key
 } pin_to_key_t;
 
 pin_to_key_t keys[] = { 
-  {5,  default_debounce, &KEY_MEDIA_PLAY_PAUSE },
-  {6,  default_debounce, &KEY_MEDIA_PREVIOUS_TRACK },
-  {7,  default_debounce, &KEY_MEDIA_NEXT_TRACK },
-  {8,  default_debounce, &KEY_MEDIA_VOLUME_DOWN },
-  {9,  default_debounce, &KEY_MEDIA_VOLUME_UP },
-  {10, default_debounce, &KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION },
+  {5,  dd, &KEY_MEDIA_PLAY_PAUSE },
+  {6,  dd, &KEY_MEDIA_PREVIOUS_TRACK },
+  {7,  dd, &KEY_MEDIA_NEXT_TRACK },
+  {8,  dd, &KEY_MEDIA_VOLUME_DOWN },
+  {9,  dd, &KEY_MEDIA_VOLUME_UP },
+  {10, dd, &KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION }, // Should open the default set music app
 };
 
-BleKeyboard bleKeyboard("Aleno Media Buttons", "Alenas", 100);
+BleKeyboard bleKeyboard("Aleno Media Buttons", "datapaganism", 100);
 
-void setup() {
 
+void setup()
+{
 
   for (int i = 0; i < (sizeof(keys) / sizeof(keys[0])); i++)
   {
@@ -44,9 +49,6 @@ void setup() {
   bleKeyboard.begin();
 }
 
-
-
-
 void loop()
 {
   if(bleKeyboard.isConnected())
@@ -55,26 +57,26 @@ void loop()
     {
 
       int reading = digitalRead(keys[i].pin);
-      if (reading != keys[i].debounce.lastButtonState)
+      if (reading != keys[i].debounce.last_button_state)
       {
-        keys[i].debounce.lastDebounceTime = millis();
+        keys[i].debounce.last_debounce_time = millis();
       }
 
-      if ((millis() - keys[i].debounce.lastDebounceTime) > debounceDelay)
+      if ((millis() - keys[i].debounce.last_debounce_time) > debounce_delay)
       {
         // if the button state has changed:
-        if (reading != keys[i].debounce.buttonState)
+        if (reading != keys[i].debounce.button_state)
         {
-          keys[i].debounce.buttonState = reading;
+          keys[i].debounce.button_state = reading;
 
-          if (keys[i].debounce.buttonState == LOW)
+          if (keys[i].debounce.button_state == LOW)
           {
             bleKeyboard.write(*keys[i].key);
           }
         }
       }
 
-      keys[i].debounce.lastButtonState = reading;
+      keys[i].debounce.last_button_state = reading;
     }
   }
 }
